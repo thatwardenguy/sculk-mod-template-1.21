8b34ee738f6da8516741a6f94052757cac7817be
Gijs van Rhijn <grhijn9379@student.landstede.nl>
2024-11-29 12:33:15 +0100

    Initial commit

---
+++ src/main/java/com/warden/sculkmod/SculkMod.java
@@ -0,0 +1,16 @@
+package com.warden.sculkmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SculkMod implements ModInitializer {
+	public static final String MOD_ID = "sculkmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/warden/sculkmod/SculkModClient.java
@@ -0,0 +1,10 @@
+package com.warden.sculkmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SculkModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
